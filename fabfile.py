752a511b2a2ae7a55beed8bc7b1d8fa014bca2cc
Walter Fan <walterfan@qq.com>
2022-05-05 23:44:42 +0800

    update fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,11 @@
 import os, sys, subprocess
 import http.server
 import socketserver
+import markdown
 import codecs
 from datetime import date
 from sys import platform
-
+from m2r import convert
 from fabric import task
 from fabric import Connection
 #from m2r import convert
@@ -12,6 +13,28 @@ from fabric import Connection
 
 BASE_PATH = os.path.dirname(__file__)
 default_hosts = ["localhost"]
+
+
+def md_to_html(md_file, html_file ):
+
+    input_file = codecs.open(md_file, mode="r", encoding="utf-8")
+    text = input_file.read()
+
+    html = markdown.markdown(text, extensions=['markdown.extensions.tables'])
+
+    output_file = codecs.open(html_file, "w", encoding="utf-8")
+    output_file.write(html)
+
+
+def md_to_rst(md_file, rst_file):
+
+    input_file = codecs.open(md_file, mode="r", encoding="utf-8")
+    text = input_file.read()
+
+    rst = convert(text)
+
+    output_file = codecs.open(rst_file, "w", encoding="utf-8")
+    output_file.write(rst)
 
 @task(hosts=default_hosts)
 def usage(c):
@@ -22,8 +45,9 @@ def usage(c):
 def md2rst(c, src, dest=None):
     if not dest:
         dest = src[:-3] + ".rst";
-    cmd = "pandoc --to RST --reference-links {} > {}".format(src, dest)
-    c.local(cmd)
+    #cmd = "pandoc --to RST --reference-links {} > {}".format(src, dest)
+    #c.local(cmd)
+    md_to_rst(src, dest)
 
 @task(hosts=default_hosts)
 def rst2md(c, src, dest=None):
@@ -36,6 +60,8 @@ def rst2md(c, src, dest=None):
 def make_note(c):
     build_cmd = 'make clean html'
     c.local(build_cmd)
+
+
 
 @task(hosts=default_hosts)
 def publish_note(c):
